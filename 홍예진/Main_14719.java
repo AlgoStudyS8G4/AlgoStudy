b1626a1455b7257d5ebab022fda7b53e31f48672
YeJinHong <teepij@naver.com>
2023-01-14 00:00:40 +0900

    modify Main_14719, not solved

--- 홍예진/Main_14719.java
+++ 홍예진/Main_14719.java
@@ -31,13 +31,24 @@ public class Main {
 				max = world[max] < world[right] ? right : max;
 				right++;
 			}
+//			System.out.println("left : "+left+" right : "+right);
 
 			// 끝까지 가도 left보다 큰 기둥은 없다.
-			if (right == W + 1) {
+			if (right == W) {
 				// left~right 사이 그나마 큰 기둥 만큼 빗물이 고인다.
-				for (int idx = left; idx < max; idx++) {
-					
+				for (int idx = left+1; idx < max; idx++) {
+					sum += world[max] - world[idx];
 				}
+				left = right;
+				continue;
+			}
+			
+			// left보다 큰 기둥이 있다. 그 사이는 left만큼 물이 찬다.
+			if(world[left] <= world[right]) {
+				for (int idx = left+1; idx < right; idx++) {
+					sum += world[left] - world[idx];
+				}
+				left = right;
 			}
 
 		}
