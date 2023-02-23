1d1c16837f577dfc1afcf0a05ed71a519c90c00b
YeJinHong <teepij@naver.com>
2023-02-23 23:55:32 +0900

    add Solution_사탕분배

---
+++ 홍예진/개인문제/Solution_사탕분배.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static int A, B, K;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for(int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			A = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+			
+			System.out.println("#"+tc+" ");
+		}
+	}
+}
