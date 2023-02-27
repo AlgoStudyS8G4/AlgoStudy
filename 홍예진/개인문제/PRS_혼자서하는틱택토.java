296a5d3fe33eef01087c509db69aefa0d279ce22
YeJinHong <teepij@naver.com>
2023-02-27 23:41:20 +0900

    add PRS_혼자서하는틱택토, not solved

---
+++ 홍예진/개인문제/PRS_혼자서하는틱택토.java
@@ -0,0 +1,46 @@
+
+class Solution {
+	boolean isBadCount(String[] board) {
+		int count_O = 0;
+		int count_X = 0;
+		for(String str : board) {
+			for(char chr : str.toCharArray()) {
+				if(chr == 'O')
+					count_O++;
+				if(chr == 'X')
+					count_X++;
+			}
+		}
+		
+		int diff = count_O - count_X;
+		if(diff == 0 || diff == 1) return false;
+		return true;
+	}
+    public int solution(String[] board) {
+        int answer = 1;
+        if(isBadCount(board)) return 0;
+        if(isEndGame(board)) return 0;
+        return answer;
+    }
+	private boolean isEndGame(String[] board) {
+		char[][] charBoard = new char[board.length][board[0].length()];
+		int idx = 0;
+		for(String str : board) {
+			if(str.equals("OOO") || str.equals("XXX")) return true;
+			charBoard[idx++] = str.toCharArray();
+		}
+		
+		for(int j = 0; j < 3; j++) {
+			String str = "";
+			for(int i = 0; i < 3; i++) {
+				str += charBoard[i][j];
+			}
+			if(str.equals("OOO") || str.equals("XXX")) return true;
+		}
+		
+		if(charBoard[0][0] == charBoard[1][1] && charBoard[1][1] == charBoard[2][2] && charBoard[1][1] != '.') return true;
+		if(charBoard[0][2] == charBoard[1][1] && charBoard[1][1] == charBoard[2][0] && charBoard[1][1] != '.') return true;
+		
+		return false;
+	}
+}
