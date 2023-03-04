379670f7d002e8bad7aaea72d5617137a9aecd7c
YeJinHong <teepij@naver.com>
2023-03-04 14:00:30 +0900

    modify PRS_전화번호목록, 효율성검사 실패

--- 홍예진/PRS_전화번호목록.java
+++ 홍예진/PRS_전화번호목록.java
@@ -10,6 +10,7 @@ class Solution {
             char pre = phone.charAt(0);
             List<String> list = map.getOrDefault(pre, new ArrayList<String>());
             for(String other : list){
+                if(other.length() > phone.length()) continue;
                 if(other.equals(phone.substring(0, other.length())))
                     return false;
             }
