9276d1a569ab9706648ccd708384a272415e5b2f
이해민 <2haemin11@gmail.com>
2023-03-25 10:22:54 +0900

    Upload PRO 구명보트

---
+++ 이해민/Solution_42885.java
@@ -0,0 +1,24 @@
+package pro.p42885;
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        
+        
+        int idx = 0;
+        
+        for(int i = people.length-1; idx <= i; i--){
+            if(people[i] + people[idx] <= limit){
+                answer++;
+                idx++;
+            }else{
+                answer++;                
+            }
+        }
+        
+        return answer;
+    }
+}
