380e896b13ffa3236b2b49c41d8323a8b2e29116
HongYejin <33932851+YeJinHong@users.noreply.github.com>
2023-03-28 23:34:31 +0900

    Update Main_23309.java, not solved. compile error

--- 홍예진/Main_23309.java
+++ 홍예진/Main_23309.java
@@ -1,5 +1,5 @@
 import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -7,58 +7,76 @@ public class Main {
 
     static class Node {
         int number;
-        Node before, after;
-        public Node(int number, Node before, Node after){
+        Node post, next;
+        public Node(int number, Node post, Node next){
             this.number = number;
-            this.before = before;
-            this.after = after;
+            this.post = post;
+            this.next = next;
         }
     }
-    public static int getBefore(LinkedList<Integer> list, int idx){
-        if(idx > 0) return list.get(idx-1);
-        else return list.getLast();
-    }
-    public static int getAfter(LinkedList<Integer> list, int idx){
-        if(idx == list.size()-1) return list.getFirst();
-        else return list.get(idx + 1);
-    }
     public static void main(String[] args){
+        Map<Integer, Node> map = new HashMap<>();
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         M = sc.nextInt();
 
-        LinkedList<Integer> list = new LinkedList<>();
-        for(int i = 0; i < N; i++){
-            list.add(sc.nextInt());
+        Node last = null;
+        Node first = null;
+        if(true){
+            int num = sc.nextInt();
+            first = new Node(num, null, null);
+            map.put(num, first);
+            last = first;
         }
+        for(int i = 1; i < N; i++){
+            int num = sc.nextInt();
+            Node now = new Node(num, last, null);
+            last.next = now;
+            map.put(num, now);
+            last = now;
+        }
+        // 입력 종료 후 first ... last 로 객체 매칭되어있음. 원형 큐 구조 반영
+        last.next = first;
+        first.post = last;
 
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i < M; i++){
             String command = sc.next();
             int num = sc.nextInt();
-            int nowIdx = list.indexOf(num);
+            Node now = map.get(num);
 
             if(command.equals("BN")){
                 int newNum = sc.nextInt();
+                Node next = now.next;
+                sb.append(next.number).append("\n");
 
-                System.out.println(getAfter(list, nowIdx));
-                if(nowIdx == list.size() -1 )
-                    list.add(newNum);
-                else
-                    list.add(nowIdx, newNum);
+                Node newNode = new Node(newNum, now, next);
+                now.next = newNode;
+                next.post = newNode;
+                map.put(newNum, newNode);
             } else if(command.equals("BP")){
                 int newNum = sc.nextInt();
-                System.out.println(getBefore(list, nowIdx));
-                list.add(nowIdx, newNum);
+                Node post = now.post;
+                sb.append(post.number).append("\n");
+
+                Node newNode = new Node(newNum, post, now);
+                now.post = newNode;
+                post.next = newNode;
+                map.put(newNum, newNode);
             }else if(command.equals("CN")){
-                System.out.println(getAfter(list, nowIdx));
-                list.remove(nowIdx+1);
+                Node next = now.next;
+                map.remove(next.number);
+                now.next = next.next;
+
+                sb.append(next.number).append("\n");
             } else { // CP
-                System.out.println(getBefore(list, nowIdx));
-                if(nowIdx == 0)
-                    list.pollLast();
-                else
-                    list.remove(nowIdx - 1);
+                Node post = now.post;
+                map.remove(post.number);
+                now.post = post.post;
+
+                sb.append(post.number).append("\n");
             }
         }
+        System.out.println(sb.toString());
     }
 }
